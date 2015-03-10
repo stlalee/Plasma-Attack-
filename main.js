e780a6162867cf261624488e053a27a211972406
Stella Lee <stjilee@ucsc.edu>
2015-03-10 02:47:19 -0700

    what am i doing

---
+++ main.js
@@ -0,0 +1,18 @@
+var game = new Phaser.Game(900, 700, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, 
+						   update: update, render: render });
+
+function preload() {
+	
+}
+
+function create() {
+	
+}
+
+function update() {
+	
+}
+
+function render() {
+	
+}
